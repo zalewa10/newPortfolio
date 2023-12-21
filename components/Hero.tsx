c4d11f5b5c647ce6ec469ccd05a8dce9c6e56566
Krzysztof <pilkarz4a@gmail.com>
2023-12-21 21:02:36 +0100

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,13 +12,13 @@ const Hero = () => {
 
   return (
     <main
-      className="min-h-screen bg-background"
+      className="min-h-screen bg-background px-4 sm:px-6 lg:px-8 "
       style={{ minHeight: "calc(100vh - 88.64px)" }}
     >
       <Container>
-        <div className="px-4 sm:px-6 lg:px-8 pt-32 flex flex-col md:flex-row items-center justify-between">
-          <div className="flex flex-col items-start justify-between gap-6">
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <div className="px-4 sm:px-6 lg:px-8 lg:pt-32 flex flex-col-reverse md:flex-row items-center justify-between">
+          <div className="flex flex-col items-center md:items-start justify-between gap-6">
+            <h1 className="scroll-m-20 text-center md:text-left text-4xl font-extrabold tracking-tight lg:text-6xl">
               Front-End React <br /> Developer{" "}
               <img
                 src="https://em-content.zobj.net/source/apple/354/waving-hand_medium-light-skin-tone_1f44b-1f3fc_1f3fc.png"
@@ -26,7 +26,7 @@ const Hero = () => {
                 alt=""
               />
             </h1>
-            <p className="text-xl text-muted-foreground max-w-lg">
+            <p className="text-xl text-muted-foreground max-w-sm text-center md:text-left lg:max-w-lg">
               Hi, my name is Krzysztof Zalewski. A passionate website developer
               based in Poznań, Poland.
             </p>
@@ -42,7 +42,7 @@ const Hero = () => {
             viewBox="0 0 548 486"
             fill={fillColor}
             xmlns="http://www.w3.org/2000/svg"
-            className="max-w-md"
+            className="max-w-72 md:max-w-xs lg:max-w-md"
           >
             <path d="M509.325 103.624C561.925 180.324 558.725 303.424 504.625 382.024C450.525 460.624 345.425 494.624 259.025 483.824C172.625 473.024 105.025 417.424 57.3248 342.624C9.72478 267.724 -17.8752 173.724 15.9248 107.824C49.8248 41.9243 145.025 4.3243 246.825 0.5243C348.625 -3.1757 456.825 26.9243 509.325 103.624Z" />
           </svg>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   return (
     <main>
-      <header className="sm:flex sm:justify-between py-3 px-4 border-b">
+      <header className="sm:flex sm:justify-between py-1 px-4 border-b">
         <Container>
           <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full">
             <div className="flex items-center">
