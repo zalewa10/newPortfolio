99a3abe40df3b6e4fd6dfcaacfc395fd630264bc
Krzysztof <61093642+zalewa10@users.noreply.github.com>
2023-12-24 15:19:47 +0100

    Add files via upload

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -8,7 +8,7 @@ const Stack = () => {
       {/* Poniżej dodać przesuwanie się Stacku */}
       <div className="flex flex-row justify-between gap-6">
         {stack.map((item, key) => (
-          <div className="flex flex-col items-center gap-1">
+          <div key={key} className="flex flex-col items-center gap-1">
             <Image src={item.imgURL} alt={item.alt} width={30} height={30} />
             <p className="text-base text-muted-foreground text-center ">
               {item.name}
